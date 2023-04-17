678ba518a0dad61451b65dd919581e55b0f8e701
Zane <zeisen33@gmail.com>
2023-04-17 07:32:17 -0400

    cors and csrf

--- backend/app.js
+++ backend/app.js
@@ -2,8 +2,14 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
+const cors = require('cors');
+const csurf = require('csurf');
+const { isProduction } = require('./config/keys');
+
 const usersRouter = require('./routes/api/users');
 const tweetsRouter = require('./routes/api/tweets')
+const csrfRouter = require('./routes/api/csrf');
+
 
 const app = express();
 
@@ -12,7 +18,25 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+if (!isProduction) {
+    // Enable CORS only in development because React will be on the React
+    // development server (http://localhost:3000). (In production, the Express 
+    // server will serve the React files statically.)
+    app.use(cors());
+}  
+
+app.use(
+    csurf({
+      cookie: {
+        secure: isProduction,
+        sameSite: isProduction && "Lax",
+        httpOnly: true
+      }
+    })
+);  
+
 app.use('/api/users', usersRouter);
 app.use('/api/tweets', tweetsRouter)
+app.use('/api/csrf', csrfRouter)
 
 module.exports = app;
