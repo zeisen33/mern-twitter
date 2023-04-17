03ee2d2626b58fb5be03644580bed9a6146f3bb6
Zane <zeisen33@gmail.com>
2023-04-17 07:57:44 -0400

    Tweets

--- backend/app.js
+++ backend/app.js
@@ -7,6 +7,13 @@ const cors = require('cors');
 const csurf = require('csurf');
 const { isProduction } = require('./config/keys');
 
+require('./models/User');
+require('./models/Tweet'); // <--ADD THIS LINE
+
+require('./models/User');
+require('./config/passport'); // <-- ADD THIS LINE
+const passport = require('passport'); // <-- ADD THIS LINE
+
 const usersRouter = require('./routes/api/users');
 const tweetsRouter = require('./routes/api/tweets')
 const csrfRouter = require('./routes/api/csrf');
@@ -18,6 +25,8 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(passport.initialize());
+
 
 if (!isProduction) {
     // Enable CORS only in development because React will be on the React
